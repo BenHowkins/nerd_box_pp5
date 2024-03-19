2538f8d41677f514276548c765c457551f1d8962
Ben Howkins <benhowkins03@hotmail.com>
2024-03-19 19:42:04 +0000

    Fixed error with product id and added search functions

--- boxes/views.py
+++ boxes/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -9,19 +11,31 @@ def all_products(request):
     """
     
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,"You Didn't Search Anything. Let's Try Again!")
+                return redirect(reverse('home'))
+        
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
 
-def product_detail(request, product_sku):
+def product_detail(request, product_id):
     """"
     A view to show all boxes
     """
     
-    product = get_object_or_404(Product, pk=product_sku)
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
         'product': product,
