1bffbb4793e20d87cc99aede10c9c30ba2e253c5
Ben Howkins <benhowkins03@hotmail.com>
2024-04-23 17:34:26 +0000

    Fix error with handler404 causing app to not deploy

--- nerd_box/settings.py
+++ nerd_box/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-benhowkins-nerdboxpp5-49mhp2nsrj4.ws-eu108.gitpod.io',
